1e56f7ce07efbaa1fd89c68a063916ac2ef16b8c
Ilya Pcn <pchelinilya13@gmail.com>
2023-01-22 15:16:41 +1100

    feat: add main func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if( str.length % 2 !== 0) return false;
+  arr1 = bracketsConfig.map( item => item.reduce( ( acc, cur) => acc+ cur))
+  let strin = str;    
+  for ( let i = 0 ; i < str.length /2 ; i++){
+      for( let j =0 ; j < arr1.length; j++){
+          strin = strin.split(arr1[j]).join('')
+      }
+  }
+  return strin.length > 0 ? false : true
 }
+// я уже делал это задание на stage 0, набор декабрь 21го
